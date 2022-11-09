775950da73ebc54596f0666164d0515438ffd00f
Muhammad Zaki <90172929+subauw@users.noreply.github.com>
2022-11-09 21:34:27 +0700

    Add files via upload

---
+++ ProductManager/src/main/java/net/codejava/Product.java
@@ -0,0 +1,70 @@
+package net.codejava;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	private Long id;
+	private String name;
+	private String brand;
+	private String madein;
+	private int price;
+
+	protected Product() {
+	}
+
+	protected Product(Long id, String name, String brand, String madein, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.madein = madein;
+		this.price = price;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getMadein() {
+		return madein;
+	}
+
+	public void setMadein(String madein) {
+		this.madein = madein;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
